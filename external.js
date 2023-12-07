3c95d75a98b7fcd97172f8bd45b5c2e95b91b757
Rosasa-2542 <rosapapawee@gmail.com>
2023-12-07 15:03:50 +0700

    exercise2-2

--- external.js
+++
@@ -1,5 +0,0 @@
-const strPlus1='Hi, my name is John';
-const strPlus2='I live in South Africa';
-const strPlus3='enjoy drinking homemade wine'
-const resultPluse = strPlus1+strPlus2+strPlus3;
-console.log(resultPluse);
